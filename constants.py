7c9753027f5759900e639382694795066024993a
Josh Faul <josh.faul@gmail.com>
2021-01-26 22:27:59 -0700

    Better labels. Yay unicode!

--- constants.py
+++ constants.py
@@ -5,9 +5,9 @@ resolutions = [
     { "name": "1080p", "width": 1920.0, "height": 1080.0 },
     { "name": "WQHD", "width": 2560.0, "height": 1440.0 },
     { "name": "UHD-1", "width": 3840.0, "height": 2160.0 },
-    { "name": "256", "width": 256.0, "height": 256.0 },
-    { "name": "512", "width": 512.0, "height": 512.0 },
-    { "name": "1024", "width": 1024.0, "height": 1024.0 }
+    { "name": "2⁸", "width": 256.0, "height": 256.0 },
+    { "name": "2⁹", "width": 512.0, "height": 512.0 },
+    { "name": "2¹⁰", "width": 1024.0, "height": 1024.0 }
 ]
 
-scales = [ ["1/4", 1/4], ["1/2", 1/2], ["1", 1], ["2", 2], ["4", 4] ]
+scales = [ ["¼", 1/4], ["½", 1/2], ["1", 1], ["2", 2], ["4", 4] ]
